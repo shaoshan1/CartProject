9b47faaaf38d7213b4edde5c10e15560b21f5fa1
shaoshan <532539937@qq.com>
2022-06-01 17:23:41 +0800

    update

---
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/YoungTest2Activity.java
@@ -0,0 +1,24 @@
+package com.xxmassdeveloper.mpchartexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class YoungTest2Activity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
