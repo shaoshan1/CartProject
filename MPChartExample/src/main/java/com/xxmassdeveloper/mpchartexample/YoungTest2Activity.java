86c63b2035d1269144158695dc9e1bbc7037910a
shaoshan <532539937@qq.com>
2022-06-01 17:52:59 +0800

    sd

--- MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/YoungTest2Activity.java
+++ MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/YoungTest2Activity.java
@@ -2,18 +2,15 @@ package com.xxmassdeveloper.mpchartexample;
 
 import android.app.Activity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import static android.content.ContentValues.TAG;
 
-=======
 
-import androidx.annotation.Nullable;
 
->>>>>>> 16b952340b0ca534a1945258a092f3fd0b94918e
+
 public class YoungTest2Activity extends Activity {
 
     @Override
@@ -24,10 +21,7 @@ public class YoungTest2Activity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-<<<<<<< HEAD
         Log.d(TAG, "onResume: ");
-=======
->>>>>>> 16b952340b0ca534a1945258a092f3fd0b94918e
     }
 
     @Override
